808df2a89e63e0d071a5d7fc82689f1fd9eaa182
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2023-11-06 22:06:51 -0300

    class 19, form of create course

--- crud-angular/src/app/courses/courses-form/courses-form.component.ts
+++ crud-angular/src/app/courses/courses-form/courses-form.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-courses-form',
   templateUrl: './courses-form.component.html',
-  styleUrls: ['./courses-form.component.scss']
+  styleUrls: ['./courses-form.component.scss'],
 })
 export class CoursesFormComponent {
+  form: FormGroup;
 
+  constructor(private formBuider: FormBuilder) {
+    this.form = this.formBuider.group({
+      name: [null],
+      category: [null],
+    });
+  }
+
+  onSubmit() {}
+  onCancel() {}
 }
