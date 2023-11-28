06e667ac342eeeeda29aabf4716f7e7db47f0628
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2023-11-27 23:28:48 -0300

    class 26, get by id

--- crud-spring/src/main/java/com/example/demo/controller/CourseController.java
+++ crud-spring/src/main/java/com/example/demo/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,12 @@ public class CourseController {
 		return courseRepository.findAll();
 	}
 
+	@GetMapping("/{idCourse}")
+	public ResponseEntity<Course> getCourseById(@PathVariable("idCourse") Long idCourse) {
+		return courseRepository.findById(idCourse).map(record -> ResponseEntity.ok().body(record))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
 	@PostMapping	
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Course createCourse(@RequestBody Course course) {
